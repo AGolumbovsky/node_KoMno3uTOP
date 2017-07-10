3f2c963d8d5f0b016c42855c1abe207c11c554cd
agolumbovsky <agolumbovsky@gmail.com>
2017-07-10 15:35:29 -0400

    mess some more

--- tester.js
+++ tester.js
@@ -1,24 +1,27 @@
 const scribble = require('scribbletune');
 
-let patterns = [
-    'x-x-x-x-x-x-x-x-',
-    'xx--xx--xx--xx--',
-    'xxx---xxx---xxx-'
-]
+let patterns = {
+    onOff:          'x-x-x-x-x-x-x-x-',
+    twoOn2Off:      'xx--xx--xx--xx--',
+    threeOn3Off:    'xxx---xxx---xxx-',
+    sixOn2Off:      'xxxxxx--xxxxxx--',
+    customX:        'xxxxxxx--xxxxxx-'
+}
 
+let repeatsNum = 8;
 // this will crate a note(s) and the pattern 
 let dumbPiano = scribble.clip({
-    notes: ['c4', 'c3', 'a2'],
-    pattern: patterns[1].repeat(8)
+    notes: ['c4', 'a2'],
+    pattern: patterns.customX.repeat(repeatsNum)
 });
 
 let bass = scribble.clip({
     notes: ['f#2'],
-    pattern: 'xx---xx---xx'.repeat(10)
+    pattern: 'xx---xx---xx'.repeat(repeatsNum)
 });
 
 // create a midi file in the same directory
 scribble.midi(dumbPiano, './clips/tester.mid');
 // scribble.midi(bass, './clips/tester.mid');
 
-console.log("the file is created/updated");
+console.log("the file is created or updated");
