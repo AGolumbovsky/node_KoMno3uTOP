52bccec44f3dc10fdb510c29459e3c0166dcf5ec
agolumbovsky <agolumbovsky@gmail.com>
2017-07-10 09:31:54 -0400

    comments

--- c.js
+++ c.js
@@ -1,7 +1,9 @@
 const scribble = require('scribbletune');
 
+// this will crate a single c4 note 
 let clip = scribble.clip({
     notes: ['c4']
 });
 
+// create a midi file in the same directory
 scribble.midi(clip);
